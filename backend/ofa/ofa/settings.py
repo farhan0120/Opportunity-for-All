14707fe6e0a7da899a04a791a27d56aedb48007a
Farhan Molla <farhanmolla@farhans-air.mynetworksettings.com>
2024-04-29 16:13:37 -0400

    changes

--- backend/ofa/ofa/settings.py
+++ backend/ofa/ofa/settings.py
@@ -123,7 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -131,9 +134,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'ofa_app/static'),
-
-    BASE_DIR / "static"
+    os.path.join(BASE_DIR, 'ofa_app', 'static')
 ]
 
 STORAGES = {
